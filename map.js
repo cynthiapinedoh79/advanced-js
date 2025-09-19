6cdc4cf67c71c2958d02164a58be839c9e2a97fc
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-09-19 00:34:24 +0000

    map method

--- map.js
+++ map.js
@@ -4,16 +4,30 @@
  */
 
 // Using a for loop
+let nums = [1,2,3,4,5];
+let results = [];
+for (let num of nums) {
+    results.push(num * 2);
+}
 
+console.log(results);
 
 // Using map()
-
+const multiplyByTwo = function(num) {
+    return num * 2;
+}
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function(num) {
+    return num * 2;
+});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
-
+const Arrow = nums.map(num => num * 2);
+console.log(Arrow);
 
 // With objects:
 const students = [
@@ -36,3 +50,20 @@ const students = [
       skill: 'CSS'
     },
   ];
+
+// Extracting specific properties as objects
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+// Destructuring in the parameter
+const newStudents = students.map(({ profession, skill, ...rest }) => rest);
+console.log(newStudents);
+
+// Adding a new property
+const ages = [32, 26, 28];
+const studentsWithAge = students.map((student, index) => ({
+  ...student, // Copies all original properties from the student
+  age: ages[index] // Adds the new age property
+}));
+
+console.log(studentsWithAge);
